e1b00ea32a1a927c2de691635a4cfe224108cf5d
Smpontelli <stefanopontelli@hotmail.com>
2020-04-16 01:31:22 -0300

    testes

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,73 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionsDTO {
+	title: string;
+
+	value: number;
+
+	type: 'income' | 'outcome';
+}
+
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+	income: number;
+	outcome: number;
+	total: number;
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+	private transactions: Transaction[];
 
-  constructor() {
-    this.transactions = [];
-  }
+	constructor() {
+		this.transactions = [];
+	}
 
-  public all(): Transaction[] {
-    // TODO
-  }
+	public all(): Transaction[] {
+		return this.transactions;
+	}
 
-  public getBalance(): Balance {
-    // TODO
-  }
+	public getBalance(): Balance {
+		const groupBy: number[] = this.transactions.reduce((acc, curr) => {
+			const key = curr.type;
+			(acc[key] = acc[key] || []).push(curr.value);
+			return acc;
+		}, Object.create(null));
 
-  public create(): Transaction {
-    // TODO
-  }
+		console.log(groupBy);
+
+		const arrayIncome: number[] = groupBy.income;
+		const arrayOutcome: number[] = groupBy.outcome;
+
+		console.log(arrayIncome);
+		console.log(arrayOutcome);
+
+		const income = arrayIncome.reduce((acc, curr) => {
+			return acc + curr;
+		});
+
+		const outcome = arrayOutcome.reduce((acc, curr) => {
+			return acc + curr;
+		});
+		console.log(income);
+		console.log(outcome);
+
+		const total = income - outcome;
+
+		const balance = {
+			income,
+			outcome,
+			total
+		};
+
+		return balance;
+	}
+
+	public create({ title, value, type }: CreateTransactionsDTO): Transaction {
+		const transaction = new Transaction({ title, value, type });
+
+		this.transactions.push(transaction);
+
+		return transaction;
+	}
 }
 
 export default TransactionsRepository;
